bf9222ea48950771421861cdb0bca903fe2e09b9
deemkj <162921660+deemkj@users.noreply.github.com>
2024-11-11 19:52:21 +0300

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author deemkj
+ */
+public class LinkedList<T> {
+    private Node<T> head;
+    private Node<T> current;
+
+    public LinkedList() {
+        head = current = null;
+    }
+
+    public boolean empty() {
+        return head == null;
+    }
+
+    public boolean last() {
+        if (current == null) {
+            return false; 
+        }
+        return current.next == null;
+    }
+}
---
+++ Node.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author deemkj
+ */
+public class Node<T> {
+public T data;
+public Node<T> next;
+public Node () {
+data = null;
+next = null;
+}
+public Node (T val) {
+data = val;
+next = null;
+}
+
+}
